e29031d22a1d641853230916215809b37e2299ca
Peter Meister <prmeister89@gmail.com>
2018-08-06 15:25:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       
       resp.write item.price
+    elsif
     else
       resp.write "Route not found"
       resp.status = 404
