789ddce2c28e38ff930d7345c856a12f5be351db
Peter Meister <prmeister89@gmail.com>
2018-08-06 15:30:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,12 +14,8 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       else
-      
-      
       resp.write item.price
-    elsif item == nil
-    
-    
+    end
     else
       resp.write "Route not found"
       resp.status = 404
