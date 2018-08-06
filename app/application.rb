b102168589a6f3a67fdc70ad134ecdf54a739ff6
Peter Meister <prmeister89@gmail.com>
2018-08-06 15:07:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,8 @@ class Application
     
     if req.path.match(/items/)
       
-      item_name = req.path.split("/items")
+      item_name = req.path.split("/items/").last
+      item = 
     else
       resp.write "Route not found"
       resp.status = 404
